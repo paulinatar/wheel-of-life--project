a026754295036536c35ebceb6b0b354a0b5f2798
paulinatar <p.tarnawska@gmail.com>
2019-08-27 16:08:10 +0200

    wheel1

---
+++ wheel_of_life/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+import {HashRouter, Link, Switch, Route} from "react-router-dom";
+import './App.scss';
+
+import Wheel from '../Wheel'
+
+function App() {
+  return (
+   <Wheel />)
+}
+
+export default App;
